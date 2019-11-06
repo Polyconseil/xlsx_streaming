f0df6fd3f87f9f15438355be0681ba93aa7331e2
Damien Baty <damien.baty@polyconseil.fr>
2019-11-06 14:50:38 +0100

    Back to development: 0.4.1

--- xlsx_streaming/__init__.py
+++ xlsx_streaming/__init__.py
@@ -1,5 +1,5 @@
 from .render import set_export_timezone
 from .streaming import stream_queryset_as_xlsx
 
-__version__ = '0.4.0'
+__version__ = '0.4.1.dev0'
 __ALL__ = ['set_export_timezone', 'stream_queryset_as_xlsx']
