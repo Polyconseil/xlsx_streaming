Polyconseil/xlsx_streaming

commit 183d794aee94e1127dd20c75a092d2ac7f5b6f77

add generator support

xlsx_streaming/streaming.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 
+from itertools import chain, islice
 import logging
 import zipfile
 
@@ -76,14 +77,15 @@ def stream_queryset_as_xlsx(
 
     return zipped_stream
 
-
 def serialize_queryset_by_batch(qs, serializer, batch_size):
-    start, last_batch = 0, []
-    while start == 0 or len(last_batch) == batch_size:
-        last_batch = list(qs[start:start + batch_size])  # force queryset evaluation
-        yield serializer(last_batch)
-        start += batch_size
+    qs_slices = chunks(qs, batch_size)
+    for batch in qs_slices:
+        yield serializer(list(batch))
 
+def chunks(iterable, size):
+    iterator = iter(iterable)
+    for first in iterator:
+        yield chain([first], islice(iterator, size - 1))
 
 def zip_to_zipstream(zip_file, only=None, exclude=None):
     """

tests/test_streaming.py
@@ -23,7 +23,7 @@ def lists_equal(list_left, list_right):
 
 class TestStreaming(unittest.TestCase):
     def test_serialize_queryset_by_batch(self):
-        queryset = [list(range(10 * i, 10 * (i + 1))) for i in range(27)]
+        queryset = [range(10 * i, 10 * (i + 1)) for i in range(27)]
         gen = streaming.serialize_queryset_by_batch(queryset, serializer=lambda x: x, batch_size=10)
 
         batch = next(gen)

commit dcf3894f1080ea64f62851706c252d104119079d

streaming: Fix bug in `stream_queryset_as_xlsx()` when used with Django QuerySet objects
183d794aee94e1127dd20c75a (version 0.4.0) accidentally changed the
behaviour of this function in some circumstances. When given a Django
QuerySet, the queryset would be evaluated only once (thus making a
single SQL query), and batched manually by the function. This could
cause performance issues if there were many rows to fetch. The
previous behaviour (which we restore here) was to perform multiple SQL
queries, actually using the `batch_size` argument of the function.

This bug was spotted by Vincent Desprez (@vinzd).

xlsx_streaming/streaming.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from itertools import chain, islice
 import logging
+import types
 import zipfile
 
 import zipstream
@@ -77,15 +78,25 @@ def stream_queryset_as_xlsx(
 
     return zipped_stream
 
-def serialize_queryset_by_batch(qs, serializer, batch_size):
-    qs_slices = chunks(qs, batch_size)
-    for batch in qs_slices:
-        yield serializer(list(batch))
 
-def chunks(iterable, size):
+def serialize_queryset_by_batch(qs, serializer, batch_size):
+    if isinstance(qs, types.GeneratorType):
+        qs_slices = _chunks(qs, batch_size)
+        for batch in qs_slices:
+            yield serializer(list(batch))
+    else:
+        start, last_batch = 0, []
+        while start == 0 or len(last_batch) == batch_size:
+            last_batch = list(qs[start:start + batch_size])  # force queryset evaluation
+            yield serializer(last_batch)
+            start += batch_size
+
+
+def _chunks(iterable, size):
     iterator = iter(iterable)
     for first in iterator:
         yield chain([first], islice(iterator, size - 1))
+
 
 def zip_to_zipstream(zip_file, only=None, exclude=None):
     """

tests/test_streaming.py
@@ -21,9 +21,25 @@ def lists_equal(list_left, list_right):
             return False
     return True
 
+
+class FakeDjangoQuerySet:
+    """Record queries (actually slices used for each query) that would be
+    made by a regular QuerySet from Django.
+    """
+    def __init__(self, values, queries):
+        self.queries = queries
+        self.values = values
+
+    def __getitem__(self, slice_):
+        # We should always be asked for a slice, never for a single item.
+        assert isinstance(slice_, slice)
+        self.queries.append(slice_)
+        return self.values[slice_]
+
+
 class TestStreaming(unittest.TestCase):
-    def test_serialize_queryset_by_batch(self):
-        queryset = [range(10 * i, 10 * (i + 1)) for i in range(27)]
+
+    def _test_serialize_queryset_by_batch(self, queryset):
         gen = streaming.serialize_queryset_by_batch(queryset, serializer=lambda x: x, batch_size=10)
 
         batch = next(gen)
@@ -39,6 +55,30 @@ class TestStreaming(unittest.TestCase):
         self.assertEqual(len(batch), 7)
 
         self.assertRaises(StopIteration, lambda: next(gen))
+
+    def test_serialize_queryset_by_batch_with_lists(self):
+        queryset = [list() for i in range(27)]
+        self._test_serialize_queryset_by_batch(queryset)
+
+    def test_serialize_queryset_by_batch_with_ranges(self):
+        queryset = [range(10 * i, 10 * (i + 1)) for i in range(27)]
+        self._test_serialize_queryset_by_batch(queryset)
+
+    def test_serialize_queryset_by_batch_with_generator(self):
+        def generate():
+            for i in range(27):
+                yield range(10 * i, 10 * (i + 1))
+        queryset = generate()
+        self._test_serialize_queryset_by_batch(queryset)
+
+    def test_serialize_queryset_django_with_queryset(self):
+        # Fake a Django QuerySet to make sure that we fetch database
+        # results by batch, not in a single query.
+        queries = []
+        values = [list(range(10 * i, 10 * (i + 1))) for i in range(27)]
+        queryset = FakeDjangoQuerySet(values, queries)
+        self._test_serialize_queryset_by_batch(queryset)
+        self.assertEqual(len(queries), 3)
 
     def test_serialize_queryset_by_batch_with_serializer(self):
         queryset = [list(range(10)) for i in range(8)]